e8c015b4a09196621cd40893c9dc95194d7306e8
(cdep)illabout <cdep.illabout@gmail.com>
2018-04-19 14:40:37 +0900

    Add twitter users to database.

--- app.hs
+++ app.hs
@@ -1,38 +1,135 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Main where
 
 import ClassyPrelude hiding (Handler)
 
-import Data.Void
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Logger
+import Data.Aeson.Lens
+import Database.Persist.Sqlite
+import Database.Persist.TH
 import Network.HTTP.Types.Status
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.HTML.Blaze
 import Text.Pretty.Simple
+import Web.Cookie
+import Web.JWT
 
-type AfterLoginApi = "after-login" :> Get '[HTML] Text
+--------
+-- DB --
+--------
+
+$(share
+  [ mkPersist sqlSettings {mpsGenerateLenses = False}
+  , mkMigrate "migrateAll"
+  ]
+  [persistLowerCase|
+  User
+    name Text
+
+    deriving Eq
+    deriving Show
+    deriving Typeable
+
+  TwitterUser
+    username Text
+    user UserId
+
+    UniqueTwitterUserUsername username
+    UniqueTwitterUserUser user
+
+    deriving Eq
+    deriving Show
+    deriving Typeable
+    |]
+ )
+
+----------------------
+-- Servant Handlers --
+----------------------
+
+instance FromHttpApiData Cookies where
+  parseHeader = Right . parseCookies
+
+instance MonadUnliftIO Handler where
+  askUnliftIO :: Handler (UnliftIO Handler)
+  -- XXX: Throw away errors:
+  askUnliftIO = pure $ UnliftIO (\(Handler (ExceptT ioEither)) -> ioEither >>= either (const (error "throw away servant err")) pure)
+
+type AfterLoginApi = "after-login" :> Header "Cookie" Cookies :> Get '[HTML] Text
 
 type HomePageApi = "index.html" :> Get '[HTML] Text
 
 type Api = AfterLoginApi :<|> HomePageApi
 
-serverRoot :: ServerT Api Handler
-serverRoot = afterLogin :<|> homePage
+serverRoot :: SqlBackend -> ServerT Api Handler
+serverRoot backend = afterLogin backend :<|> homePage backend
 
-afterLogin :: Handler Text
-afterLogin = undefined
+afterLogin :: SqlBackend -> Maybe Cookies -> Handler Text
+afterLogin backend cookies = do
+  let header = ("Location", "https://servant-and-login-with.com:8443/index.html")
+  case cookies >>= lookup "jwt" of
+    Nothing -> error "no jwt cookie"
+    Just jwt -> do
+      pPrint jwt
+      let textJWT = decodeUtf8 jwt
+      let maybeVerifiedJWT = decodeAndVerifySignature (secret "foobarbaz") textJWT
+      putStrLn "maybeVerifiedJWT:"
+      pPrint maybeVerifiedJWT
+      case maybeVerifiedJWT of
+        Nothing -> error "could not verify jwt"
+        Just verifiedJWT -> do
+          let jwtClaims = unregisteredClaims $ claims verifiedJWT
+          case lookup "profile" jwtClaims of
+            Nothing -> error "could not find the twitter profile in jwt claims"
+            Just profileJSON ->
+              case profileJSON ^? key "username" . _String of
+                Nothing -> error "could not find the username key in the profile"
+                Just username -> do
+                  flip runSqlConn backend $ do
+                    userKey <- insert $ User "fred"
+                    insert_ $ TwitterUser username userKey
+                  throwError $ err302 { errHeaders = [header] }
 
-homePage :: Handler Text
-homePage = pure "<p>hello world</p>"
+homePage :: SqlBackend -> Handler Text
+homePage backend = do
+  (twitterUsers, users) <- flip runSqlConn backend $ do
+    (twitterUsers :: [Entity TwitterUser]) <- selectList [] []
+    (users :: [Entity User]) <- selectList [] []
+    pure (twitterUsers, users)
+  putStrLn "twitter users:"
+  pPrint twitterUsers
+  putStrLn "users:"
+  pPrint users
+  pure "<p>hello world</p>"
 
-app :: Application
-app = serveWithContext (Proxy @Api) EmptyContext serverRoot
+----------
+-- Main --
+----------
+
+instance MonadLogger IO where
+  monadLoggerLog a b c d = runStdoutLoggingT $ monadLoggerLog a b c d
+
+app :: SqlBackend -> Application
+app = serveWithContext (Proxy @Api) EmptyContext . serverRoot
 
 -- app :: Application
 -- app req respFunc = do
@@ -40,4 +137,7 @@ app = serveWithContext (Proxy @Api) EmptyContext serverRoot
 --   respFunc $ responseLBS status200 [] "<p>hello</p>"
 
 main :: IO ()
-main = run 8000 app
+main =
+  withSqliteConn ":memory:" $ \backend -> do
+    runSqlConn (runMigration migrateAll) backend
+    run 8000 $ app backend
