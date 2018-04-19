3fa7821814a396afb85440135b4f091428a9567b
(cdep)illabout <cdep.illabout@gmail.com>
2018-04-19 15:00:59 +0900

    Add in UserID claim.

--- app.hs
+++ app.hs
@@ -21,7 +21,9 @@ import ClassyPrelude hiding (Handler)
 import Control.Lens
 import Control.Monad.Except
 import Control.Monad.Logger
+import Data.Aeson (Value(Number))
 import Data.Aeson.Lens
+import Data.ByteString.Builder
 import Database.Persist.Sqlite
 import Database.Persist.TH
 import Network.HTTP.Types.Status
@@ -72,7 +74,12 @@ instance FromHttpApiData Cookies where
 instance MonadUnliftIO Handler where
   askUnliftIO :: Handler (UnliftIO Handler)
   -- XXX: Throw away errors:
-  askUnliftIO = pure $ UnliftIO (\(Handler (ExceptT ioEither)) -> ioEither >>= either (const (error "throw away servant err")) pure)
+  askUnliftIO =
+    pure $
+      UnliftIO
+        (\(Handler (ExceptT ioEither)) ->
+            ioEither >>= either (const (error "throw away servant err")) pure
+        )
 
 type AfterLoginApi = "after-login" :> Header "Cookie" Cookies :> Get '[HTML] Text
 
@@ -84,30 +91,50 @@ serverRoot :: SqlBackend -> ServerT Api Handler
 serverRoot backend = afterLogin backend :<|> homePage backend
 
 afterLogin :: SqlBackend -> Maybe Cookies -> Handler Text
-afterLogin backend cookies = do
-  let header = ("Location", "https://servant-and-login-with.com:8443/index.html")
-  case cookies >>= lookup "jwt" of
-    Nothing -> error "no jwt cookie"
-    Just jwt -> do
-      pPrint jwt
-      let textJWT = decodeUtf8 jwt
-      let maybeVerifiedJWT = decodeAndVerifySignature (secret "foobarbaz") textJWT
-      putStrLn "maybeVerifiedJWT:"
-      pPrint maybeVerifiedJWT
-      case maybeVerifiedJWT of
-        Nothing -> error "could not verify jwt"
-        Just verifiedJWT -> do
-          let jwtClaims = unregisteredClaims $ claims verifiedJWT
-          case lookup "profile" jwtClaims of
-            Nothing -> error "could not find the twitter profile in jwt claims"
-            Just profileJSON ->
-              case profileJSON ^? key "username" . _String of
-                Nothing -> error "could not find the username key in the profile"
-                Just username -> do
-                  flip runSqlConn backend $ do
-                    userKey <- insert $ User "fred"
-                    insert_ $ TwitterUser username userKey
-                  throwError $ err302 { errHeaders = [header] }
+afterLogin backend maybeCookies = do
+  case maybeCookies of
+    Nothing -> error "no cookies"
+    Just cookies ->
+      case lookup "jwt" cookies of
+        Nothing -> error "no jwt cookie"
+        Just jwt -> do
+          pPrint jwt
+          let textJWT = decodeUtf8 jwt
+              secretKey = secret "foobarbaz"
+          let maybeVerifiedJWT = decodeAndVerifySignature secretKey textJWT
+          putStrLn "maybeVerifiedJWT:"
+          pPrint maybeVerifiedJWT
+          case maybeVerifiedJWT of
+            Nothing -> error "could not verify jwt"
+            Just verifiedJWT -> do
+              let jwtClaims = unregisteredClaims $ claims verifiedJWT
+              case lookup "profile" jwtClaims of
+                Nothing -> error "could not find the twitter profile in jwt claims"
+                Just profileJSON ->
+                  case profileJSON ^? key "username" . _String of
+                    Nothing -> error "could not find the username key in the profile"
+                    Just username -> do
+                      userKey <- flip runSqlConn backend $ do
+                        userKey <- insert $ User "fred"
+                        insert_ $ TwitterUser username userKey
+                        pure userKey
+                      let newClaims = insertMap "userid" (Number $ fromIntegral $ fromSqlKey userKey) jwtClaims
+                          newClaimsSet = (claims verifiedJWT) { unregisteredClaims = newClaims }
+                          newJWT = encodeUtf8 $ encodeSigned HS256 secretKey newClaimsSet
+                          newSetCookie =
+                            defaultSetCookie
+                              { setCookieName = "jwt"
+                              , setCookieValue = newJWT
+                              , setCookiePath = Just "/"
+                              , setCookieDomain = Just ".servant-and-login-with.com"
+                              , setCookieHttpOnly = True
+                              , setCookieSecure = True
+                              }
+                      let headers =
+                            [ ("Set-Cookie", toStrict . toLazyByteString $ renderSetCookie newSetCookie)
+                            , ("Location", "https://servant-and-login-with.com:8443/index.html")
+                            ]
+                      throwError $ err302 { errHeaders = headers }
 
 homePage :: SqlBackend -> Handler Text
 homePage backend = do
